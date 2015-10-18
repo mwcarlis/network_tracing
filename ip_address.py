9c6c718a6fc054d77827d3c0998442d2bdec73ea
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-10-18 15:57:59 -0700

    packet_sniff: Create a packet sniffing object.
    Using a threaded packet sniffing object.

---
+++ ip_address.py
@@ -0,0 +1,10 @@
+"""An IPAddress Module.
+"""
+
+class IPAddress(object):
+    def __init__(self, ip):
+        self.ip = ip
+
+if __name__ == '__main__':
+    ipa = IPAddress()
+
--- packet_sniff.py
+++ packet_sniff.py
@@ -7,60 +7,126 @@ Sniffs only incoming TCP packet
 
 import socket, sys
 from struct import *
+from collections import namedtuple
 
-#create an INET, STREAMing socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-except socket.error , msg:
-    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+import threading
+import Queue
 
-# receive a packet
-while True:
-    packet = s.recvfrom(65565)
 
+PACKET_ELEMS = [
+    'version', 'ip_header_length', 'ttl',
+    'protocol', 'src_address', 'dest_address',
+    'src_port', 'dest_port', 'sequence_number',
+    'ack', 'tcp_header_length'
+]
+PACKET = namedtuple('packet', PACKET_ELEMS)
+
+
+def receive_packet(sock):
+    """A function to receive a packet.
+    """
+    packet = sock.recvfrom(65565)
     #packet string from tuple
     packet = packet[0]
 
     #take first 20 characters for the ip header
     ip_header = packet[0:20]
-
-    #now unpack them :)
     iph = unpack('!BBHHHBBH4s4s' , ip_header)
-
     version_ihl = iph[0]
-    version = version_ihl >> 4
+
+    # Interesting IP items unpacked.
+    pkt_version = version_ihl >> 4
     ihl = version_ihl & 0xF
-
-    iph_length = ihl * 4
-
-    ttl = iph[5]
-    protocol = iph[6]
+    ttl_num = iph[5]
+    protocol_type = iph[6]
     s_addr = socket.inet_ntoa(iph[8]);
     d_addr = socket.inet_ntoa(iph[9]);
 
-    print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-
+    # Get the TCP Header
+    iph_length = ihl * 4
     tcp_header = packet[iph_length:iph_length+20]
-
-    #now unpack them :)
     tcph = unpack('!HHLLBBHHH' , tcp_header)
 
-    source_port = tcph[0]
-    dest_port = tcph[1]
+    # Interesting TCP items unpacked.
+    source_port_num = tcph[0]
+    dest_port_num = tcph[1]
     sequence = tcph[2]
     acknowledgement = tcph[3]
     doff_reserved = tcph[4]
     tcph_length = doff_reserved >> 4
 
-    print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
-    print '\n'
-
+    # Get the data segment of the Packet.
     # h_size = iph_length + tcph_length * 4
     # data_size = len(packet) - h_size
-    #
     # #get data from the packet
     # data = packet[h_size:]
-    #
-    # print 'Data : ' + data
-    # print
+
+    # Pack up the packet details.
+    pkt_obj = PACKET(version=pkt_version,
+                     ip_header_length=ihl,
+                     ttl=ttl_num,
+                     protocol=protocol_type,
+                     src_address=s_addr,
+                     dest_address=d_addr,
+                     src_port=source_port_num,
+                     dest_port=dest_port_num,
+                     sequence_number=sequence,
+                     ack=acknowledgement,
+                     tcp_header_length=tcph_length)
+    return pkt_obj
+
+class PacketSniffer(threading.Thread):
+
+    def __init__(self, shared_queue=None):
+        super(PacketSniffer, self).__init__()
+        self.alive = threading.Event()
+        self.alive.set()
+        self.start()
+
+        if isinstance(queue, Queue.Queue):
+            self.shared_queue = shared_queue
+        else:
+            self.shared_queue = Queue.Queue()
+
+    def get_queue(self):
+        return self.shared_queue
+
+    def connect(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        except socket.error, msg:
+            self.alive.clear()
+            raise
+
+    def run(self):
+        self.connect()
+
+        while self.alive.isSet():
+            pkt_obj = receive_packet(self.sock)
+            # Send the packet details upstream.
+            self.shared_queue.put(pkt_obj)
+
+
+def test_receive(max_packets=100):
+    """Receive a number (max_packets) of packets as a test and print to stout.
+    """
+    try:
+        #create an INET, STREAMing socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    except socket.error , msg:
+        message = 'Socket could not be created. Error Code : '
+        print message + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+
+    counter = 0
+    while counter < max_packets:
+        # receive a packet
+        pkt_obj = receive_packet(sock)
+        print pkt_obj.__repr__() + '\n'
+        counter += 1
+    print 'TEST COMPLETE'
+
+
+if __name__ == '__main__':
+    test_receive()
+
