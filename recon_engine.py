feea7702980bbbd861807b06b351292064df90ac
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-11-28 13:55:01 -0800

    recon_engine: simplify ReconProtocol.
    The initial implementation of ReconProtocol was
    far too complex to manage.  Simplify this protocol
    with switch statements.

--- recon_engine.py
+++ recon_engine.py
@@ -9,6 +9,7 @@ from region_trace import TraceRoute
 from network_packets.dns_parser import DNSResolver
 from ip_address import IPAddress
 
+
 LOCALHOST = ('localhost', 5555)
 MAX_MSG = 1024
 
@@ -22,9 +23,6 @@ class MockIface(object):
     def recv(self, num):
         raise Exception('Mock Recv')
 
-def assign_none(blob):
-    blob = None
-
 
 class ReconProtocol(object):
     # Protocol argument flags.
@@ -32,61 +30,43 @@ class ReconProtocol(object):
     ARG_IFACE   = 1
     ARG_PAYLOAD = 2
     ARG_MESSAGE = 3
+    ARG_RECV    = 4
 
     def __init__(self, proto_side='client'):
         self.iface = MockIface()
-        SERVER_CMDS = {
-            'stop': (
-                (self.ARG_NONE, self.iface.close),
-                (self.ARG_IFACE, assign_none),
-            ),
-            'echo': (
-                (self.ARG_PAYLOAD, self.iface.sendall),
-            ),
-        }
-        CLIENT_CMDS = {
-            'stop': (
-                (self.ARG_NONE, self.iface.close),
-                (self.ARG_IFACE, assign_none),
-            ),
-            'echo': (
-                (self.ARG_PAYLOAD, self.iface.sendall),
-            ),
-            'send_recv': (
-                (self.ARG_MESSAGE, self.iface.sendall),
-                (self.ARG_MESSAGE, self.iface.recv),
-            ),
-        }
         if proto_side == 'client':
-            self.proto_cmds = CLIENT_CMDS
+            self.parse_cmd = self.parse_client
         elif proto_side == 'server':
-            self.proto_cmds = SERVER_CMDS
+            self.parse_cmd = self.parse_server
 
     def new_iface(self, iface):
-        print 'setting new iface'
         self.iface = iface
 
-    def parse_cmd(self, message=''):
-        # Parse the message into a cmd, and a payload.
-        try:
-            cmd, payload = tuple(message.split('-'))
-            # Get the command sequence for this cmd.
-            cmd_sequence = self.proto_cmds[cmd]
-            # Take appropriate action for this command.
-            for arg, runnable_cmd in cmd_sequence:
-                if arg == self.ARG_NONE:
-                    runnable_cmd()
-                elif arg == self.ARG_IFACE:
-                    runnable_cmd(self.iface)
-                elif arg == self.ARG_PAYLOAD:
-                    runnable_cmd(message)
-                elif arg == self.ARG_MESSAGE:
-                    runnable_cmd(message)
-                else:
-                    print 'cmd_error:', message
-        except KeyError:
-            print 'proto_error:', message
+    def parse_client(self, message=''):
+        items = message.split('-')
+        if 'cm.echo' in items:
+            reply = 'cm.print-{}'.format(items[-1:])
+            self.iface.sendall(reply)
+        elif 'cm.print' in items:
+            print message
+        elif 'cm.stop' in items:
+            self.iface.close()
+            self.iface = None
 
+    def parse_server(self, message=''):
+        items = message.split('-')
+        if 'cm.echo' in items:
+            reply = 'cm.print-{}'.format(items[-1:])
+            self.iface.sendall(reply)
+        elif 'cm.print' in items:
+            print message
+            reply = 'cm.stop-{}'.format(items[-1:])
+            self.iface.sendall(reply)
+        elif 'cm.stop' in items:
+            reply = 'cm.stop-dummy'
+            self.iface.sendall(reply)
+            self.iface.close()
+            self.iface = None
 
 
 class ReconClient(object):
@@ -107,9 +87,10 @@ class ReconClient(object):
         if len(message) > MAX_MSG:
             errmsg = 'message too large: msg-{} > MAX-{}'
             raise Exception(errmsg).format(len(message), MAX_MSG)
+        # Send a message.
         self.sock.sendall(message)
-        ret_msg = self.sock.recv(MAX_MSG)
-        self.protocol.parse_cmd(ret_msg)
+        # Receive a response and parse it.
+        self.protocol.parse_cmd(self.sock.recv(MAX_MSG))
 
 class ReconServer(threading.Thread):
     def __init__(self, shared_queue=None):
